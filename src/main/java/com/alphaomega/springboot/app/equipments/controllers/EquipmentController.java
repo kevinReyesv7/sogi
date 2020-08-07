f15f882be3ba232562fa353e805034fd51e4956a
digutier <66893053+digutier@users.noreply.github.com>
2020-08-07 04:01:37 -0400

    Intento config CORS

--- src/main/java/com/alphaomega/springboot/app/equipments/controllers/EquipmentController.java
+++ src/main/java/com/alphaomega/springboot/app/equipments/controllers/EquipmentController.java
@@ -5,6 +5,7 @@ import java.net.URISyntaxException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
