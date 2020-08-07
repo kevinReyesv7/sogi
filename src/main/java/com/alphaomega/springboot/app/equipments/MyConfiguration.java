5cdce4cd8c6a245acc1265765fee0b12968fabf9
digutier <66893053+digutier@users.noreply.github.com>
2020-08-07 05:30:38 -0400

    Se agrega para configurar CORS

---
+++ src/main/java/com/alphaomega/springboot/app/equipments/MyConfiguration.java
@@ -0,0 +1,21 @@
+package com.alphaomega.springboot.app.equipments;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+}
