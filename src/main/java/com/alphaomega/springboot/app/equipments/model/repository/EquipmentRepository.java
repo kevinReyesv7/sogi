098441a8b57b6819bb3ab70011fed7be9fec8747
digutier <66893053+digutier@users.noreply.github.com>
2020-08-06 20:16:35 -0400

    Se agrega java.util.list

--- src/main/java/com/alphaomega/springboot/app/equipments/model/repository/EquipmentRepository.java
+++ src/main/java/com/alphaomega/springboot/app/equipments/model/repository/EquipmentRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.alphaomega.springboot.app.equipments.model.entity.Equipment;
 
+import java.util.List;
+
 public interface EquipmentRepository extends CrudRepository<Equipment, Long>{
   List<Equipment> findByIsDeletedFalse();
 }
