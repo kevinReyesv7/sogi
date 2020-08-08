6f0e3b067845038cb82658de9732f3b25f5a4e11
digutier <66893053+digutier@users.noreply.github.com>
2020-08-08 06:19:31 -0400

    Identación

--- src/main/java/com/alphaomega/springboot/app/equipments/controllers/EquipmentController.java
+++ src/main/java/com/alphaomega/springboot/app/equipments/controllers/EquipmentController.java
@@ -51,71 +51,51 @@ public class EquipmentController {
 	
 	@PostMapping(value = "/equipamiento")
 	public ResponseEntity<Equipment> addEquipment(@Validated @RequestBody Equipment equipment) 
-
             throws URISyntaxException {
-
-        try {
-
-            Equipment newEquipment = equipmentService.save(equipment);
-
-            return ResponseEntity.created(new URI("/api/equipments/" + newEquipment.getId()))
-
-                    .body(equipment);
-
-        } catch (ResourceAlreadyExistsException ex) {
-
-            return ResponseEntity.status(HttpStatus.CONFLICT).build();
-
-        } catch (BadResourceException ex) {
-
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-
-        }
-
-    }
+        	try {
+            		Equipment newEquipment = equipmentService.save(equipment);
+            		return ResponseEntity.created(new URI("/api/equipments/" + newEquipment.getId()))
+                    	.body(equipment);
+        	}
+		catch (ResourceAlreadyExistsException ex) {
+            		return ResponseEntity.status(HttpStatus.CONFLICT).build();
+		}
+        	catch (BadResourceException ex) {
+            		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        	}
+	    }
 	
 	
 	@PatchMapping(value = "/equipamiento/{id}")
 	public ResponseEntity<Object> partialUpdateName(@Validated @RequestBody Map<Object, Object> fields, @PathVariable long id) {
-        try {
-	    Equipment equipment = equipmentService.findById(id);
-            fields.forEach((k, v) -> {
-                Field field = ReflectionUtils.findRequiredField(Equipment.class, (String) k);
-                field.setAccessible(true);
-                ReflectionUtils.setField(field, equipment, v);
-            });
-            equipment.setId(id);
-
-            equipmentService.update(equipment);
-
-            return ResponseEntity.ok().build();
-
-        } catch (ResourceNotFoundException ex) {
-
-
-            return ResponseEntity.notFound().build();
-
-        } catch (BadResourceException ex) {
-
-
-
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-
-        }
+        	try {
+	    		Equipment equipment = equipmentService.findById(id);
+            		fields.forEach((k, v) -> {
+                		Field field = ReflectionUtils.findRequiredField(Equipment.class, (String) k);
+                		field.setAccessible(true);
+                		ReflectionUtils.setField(field, equipment, v);
+            		});
+            		equipment.setId(id);
+            		equipmentService.update(equipment);
+            		return ResponseEntity.ok().build();
+       		}
+		catch (ResourceNotFoundException ex) {
+            		return ResponseEntity.notFound().build();
+        	}
+		catch (BadResourceException ex) {
+            		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        	}
 	}
 	
 			
 	@DeleteMapping("/equipamiento/{id}")
     	public ResponseEntity<Void> deleteEquipmentById(@PathVariable long id) {
-        try {
-            	equipmentService.delete(id);
-            return ResponseEntity.ok().build();
-
-        } catch (ResourceNotFoundException ex) {
-
-
-            return ResponseEntity.notFound().build();
-
-        }
+        	try {
+            		equipmentService.delete(id);
+            		return ResponseEntity.ok().build();
+        	}
+		catch (ResourceNotFoundException ex) {
+            		return ResponseEntity.notFound().build();
+        	}
     	}
 }
