c21b565c5b80c2e57d80d4309b3040926cfcff5f
digutier <66893053+digutier@users.noreply.github.com>
2020-08-08 02:17:02 -0400

    Se cambia PUT por PATCH

--- src/main/java/com/alphaomega/springboot/app/equipments/controllers/EquipmentController.java
+++ src/main/java/com/alphaomega/springboot/app/equipments/controllers/EquipmentController.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.utils.ReflectionUtils;
+import org.springframework.data.util.ReflectionUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
